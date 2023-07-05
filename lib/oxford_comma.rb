15c32777ebbed6c628bd3584fd5883986682594a
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-07-05 18:07:52 +0300

    first commit

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
-end
+    case array.length
+    when 1
+      array[0]
+    when 2
+      array.join(" and ")
+    else
+      last_element = array.pop
+      array.join(", ") + ", and " + last_element
+    end
+  end
+  
